a7cb895c53107470b96fd45f2904973423368fdc
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2018-10-25 15:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  const testArray = [];
+
+  for(let i = 0, l = musicians.length ; i < l; i++){
+    testArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return testArray;
+}
+
+function johnLennonFacts(facts) {
+  const outputFacts = [];
+
+  let i = 0;
+  while (i < facts.length) {
+    outputFacts.push(`${facts[i]}!!!`);
+    i++;
+  }
+
+  return outputFacts;
+}
+
